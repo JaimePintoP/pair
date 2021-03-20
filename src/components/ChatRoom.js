05c197b68b9598531e0b7c4d2a329863eb2a888c
JaimePintoP <jaimepinto.saxo@gmail.com>
2021-03-20 15:34:59 +0100

    Add ChatRoom Component

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,6 +1,9 @@
 import React from "react";
+import openSocket from "socket.io-client";
 
 const ChatRoom = () => {
+  const socket = openSocket("http://localhost:4444");
+
   return <div>ChatRoom</div>;
 };
 
